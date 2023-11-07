6638d1b3d65a720182eb3ae6f6729cfc5b41ea35
enriquerocha23 <127061710+enriquerocha23@users.noreply.github.com>
2023-11-06 19:26:26 -0600

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,37 @@
+function sumar(){
+    let n1, n2, sumar
+    n1 = prompt("Escribe el primer numero")
+    n2 = prompt("Escribe el segundo numero")
+
+    sumar = parseInt(n1) + parseInt(n2)
+
+    alert(sumar)
+}
+function restar(){
+    let nu1, nu2, restar
+    nu1 = prompt("Escribe el primer numero")
+    nu2 = prompt("Escribe el segundo numero")
+
+    restar= parseInt(nu1) - parseInt(nu2)
+
+    alert(restar)
+}
+
+function  mutiplicar(){
+    let n1, n2, mutiplicar
+    n1 = prompt("Escribe el primer numero")
+    n2 = prompt("Escribe el segundo numero")
+
+    mutiplicar = parseInt(n1) * parseInt(n2)
+
+    alert(mutiplicar)
+}
+function dividir(){
+    let n1, n2, dividir
+    n1 = prompt("Escribe el primer numero")
+    n2 = prompt("Escribe el segundo numero")
+
+    dividir = parseInt(n1) / parseInt(n2)
+
+    alert(dividir)
+}
